98f54cb60a454eb5b5dd9056429af189069e2050
Rakesh <rakeshnagarajan04@gmail.com>
2025-03-29 11:09:44 +0530

    new update

---
+++ app.py
@@ -0,0 +1,14 @@
+# Import Flask
+from flask import Flask
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+# Define the homepage route
+@app.route('/')
+def home():
+    return "Hello, welcome to my Flask app!"
+
+# Start the Flask development server
+if __name__ == '__main__':
+    app.run(debug=True)
